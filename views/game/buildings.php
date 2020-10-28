0da9ea2bf14e7856f7d94ca97841bac345c8ba52
Tsume <tsume@misan.misan.net.pl>
2020-10-28 16:55:23 +0100

    Poprawka w menu budynkow i wstep do produkcji

--- views/game/buildings.php
+++ views/game/buildings.php
@@ -1,10 +1,17 @@
+<?php
+	$building = isset($_GET['b']) ? $_GET['b'] : 'resources';
+?>
+
 <div class="buildings">
 	<ul>
 		<li class="building">
-			<a class="<?php echo($_GET['b'] == 'resources' ? 'active' : ''); ?>" href="?v=dashboard&b=resources">Magazyn</a>
+			<a class="<?php echo($building == 'resources' ? 'active' : ''); ?>" href="?v=dashboard&b=resources">Magazyn</a>
 		</li>
 		<li>
-			<a class="<?php echo($_GET['b'] == 'laboratorie' ? 'active' : ''); ?>" href="?v=dashboard&b=laboratorie">Laboratorium</a>
+			<a class="<?php echo($building == 'laboratorie' ? 'active' : ''); ?>" href="?v=dashboard&b=laboratorie">Laboratorium</a>
+		</li>
+		<li>
+			<a class="<?php echo($building == 'production' ? 'active' : ''); ?>" href="?v=dashboard&b=production">Produkcja</a>
 		</li>
 	</ul>
 </div>
---
+++ views/game/production.php
@@ -0,0 +1,32 @@
+<div id="production">
+	<div class="">
+		Dom drwala
+		<span>Poziom 1</span>
+		<p>Produkcja 10 / godz.</p>
+		<button disabled>Rozbuduj</button>
+	</div>
+	<div class="">
+		Tartak
+		<span>Poziom 1</span>
+		<p>Produkcja 10 / godz.</p>
+		<button disabled>Rozbuduj</button>
+	</div>
+	<div class="">
+		Kamieniołom
+		<span>Poziom 1</span>
+		<p>Produkcja 10 / godz.</p>
+		<button disabled>Rozbuduj</button>
+	</div>
+	<div class="">
+		Studnia
+		<span>Poziom 1</span>
+		<p>Produkcja 10 / godz.</p>
+		<button disabled>Rozbuduj</button>
+	</div>
+	<div class="">
+		Farma
+		<span>Poziom 1</span>
+		<p>Produkcja 10 / godz.</p>
+		<button disabled>Rozbuduj</button>
+	</div>
+</div>
